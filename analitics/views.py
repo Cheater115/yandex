3c5f70efc562ca5abe4f73d5c0441cabb5e2b82b
Tolstyakov Sergey <angryshikamaru@gmail.com>
2019-08-18 12:35:11 +0300

    write views for /imports

--- analitics/views.py
+++ analitics/views.py
@@ -1,26 +1,50 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import status, serializers
+
+
+from analitics.models import Import, Citizen
+from analitics.serializers import ImportSerializer, CitizenSerializer
 
 
 class ImportCreate(APIView):
     parser_classes = (JSONParser,)  # принимать только json
     def post(self, request):
-        return Response(status=status.HTTP_200_OK)
+        serializer = ImportSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
 
 
-class CititzenDetail(APIView):
+class CitizenDetail(APIView):
     def patch(self, request, import_id, citizen_id):
-        pass
+        try:
+            imp = Import.objects.get(id=import_id)
+        except Import.DoesNotExist:
+            raise serializers.ValidationError('hasnt such import')
+        try:
+            citizen = imp.citizen_set.get(citizen_id=citizen_id)
+        except Citizen.DoesNotExist:
+            raise serializers.ValidationError('hasnt such citizen')
+        # update
+        # Update `comment` with partial data
+        # serializer = CommentSerializer(comment, data={'content': u'foo bar'}, partial=True)
 
 
-class CititzenList(APIView):
+class CitizenList(APIView):
     def get(self, request, import_id):
-        pass 
+        try:
+            imp = Import.objects.get(id=import_id)
+        except Import.DoesNotExist:
+            raise serializers.ValidationError('hasnt such import')
+        citizens = imp.citizen_set.all()
+        # citizens = Citizen.objects.filter(import_id=import_id) 
+        serializer = CitizenSerializer(citizens, many=True)
+        return Response(serializer.data)
 
 
-class CititzenBirthdays(APIView):
+class CitizenBirthdays(APIView):
     def get(self, request, import_id):
         pass
 
